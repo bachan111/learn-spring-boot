b5fdfb3b5f2425f0e4a20dcb7a82bbb6f5214eb0
bachan <changbaofeng@travelsky.com.cn>
2022-08-16 15:02:36 +0800

    【springboot】 全局异常处理

--- helloWorld/src/main/java/com/bachan/controller/HelloController.java
+++ helloWorld/src/main/java/com/bachan/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.bachan.controller;
 
+import com.bachan.result.BizException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Locale;
+
 @Controller
 public class HelloController {
     Logger logger = LoggerFactory.getLogger(getClass());
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
@@ -18,6 +22,14 @@ public class HelloController {
         logger.info("info 级别日志");
         logger.warn("warn 级别日志");
         logger.error("error 级别日志");
+        if (true){
+//            throw new BizException("-1", "用户姓名不能为空！");
+
+//            String str = null;
+//            str.equals("111");
+
+            Integer.parseInt("abc123");
+        }
         return "Hello World!";
     }
 }
