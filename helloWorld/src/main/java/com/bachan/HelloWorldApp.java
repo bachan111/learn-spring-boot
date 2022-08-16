020d1721393263c1690d02e764879cf6ea9ba8f1
bachan <changbaofeng@travelsky.com.cn>
2022-08-16 11:23:53 +0800

    【springboot】 log日志配置

--- helloWorld/src/main/java/com/bachan/HelloWorldApp.java
+++ helloWorld/src/main/java/com/bachan/HelloWorldApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApp {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApp.class, args);
+
     }
 }
--- helloWorld/src/main/java/com/bachan/controller/HelloController.java
+++ helloWorld/src/main/java/com/bachan/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.bachan.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(getClass());
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
+        //日志级别 由低到高
+        logger.trace("trace 级别日志");
+        logger.debug("debug 级别日志");
+        logger.info("info 级别日志");
+        logger.warn("warn 级别日志");
+        logger.error("error 级别日志");
         return "Hello World!";
     }
 }
